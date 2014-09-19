e97a84ba821a7681358b9f6913f58ff2e3c5c44a
Keav <chris@keav.net>
2014-09-20 00:13:01 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+         <title>KEAV</title>
+         
+         <meta name="robots" content="NOINDEX,NOFOLLOW" />
+         <meta name="revisit-after" content="7 days" />
+
+         <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> -->
+
+         <link href="style.css" rel="stylesheet" type="text/css" />
+
+         <script>
+            var _gaq = _gaq || [];
+            var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+            _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+            _gaq.push(['_setAccount', 'UA-42923324-2']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+          </script>
+
+<!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<header>
+<span><?php echo date("l jS F Y H:i"); ?></span>
+</header>
+
+<div id="logo">
+<img src="images/keav_new_logo.jpg" alt="logo" title="logo" />
+</div>
+
+<footer>
+<span id="copy">Copyright &#169; 2013 <a href="&#109;&#x61;&#105;&#x6c;&#116;&#111;&#x3a;&#99;&#104;&#114;&#x69;&#x73;&#x40;&#107;&#101;&#x61;&#x76;&#46;&#110;&#x65;&#x74;">Chris Keavey</a>. All rights reserved.</span>
+<span id="valid"><a href="http://validator.w3.org/check/referer" title="Valid HTML5!" rel="nofollow">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer" title="Valid CSS3!">CSS3</a></span>
+</footer>
+
+<script src="js/analytics.js"></script>
+</body>
+</html>
